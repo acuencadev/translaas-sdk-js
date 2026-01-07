814f6bb976fe2e608c0c42fa27042b002d358c54
Amador Cuenca <sphi02ac@gmail.com>
2026-01-07 17:01:07 -0300

    [Chore] Clean Up Dist Files and Update .gitignore
    * Remove generated dist files for various packages including caching, client, core, and extensions to ensure a cleaner repository.
    * Update .gitignore to exclude root-level dist files while retaining package-specific dist folders for npm publishing.
    * This cleanup helps maintain a tidy project structure and reduces unnecessary clutter in the version control system.

--- packages/@translaas/core/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from '@translaas/models';
-export * from '@translaas/client';
-export * from '@translaas/caching';
-export * from '@translaas/caching-file';
-export * from '@translaas/extensions';
-//# sourceMappingURL=index.d.ts.map
--- packages/@translaas/extensions/dist/types.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ILanguageProvider {
-    getLanguageAsync(): Promise<string | null>;
-}
-export interface ILanguageResolver {
-    resolveLanguageAsync(): Promise<string | null>;
-}
-//# sourceMappingURL=types.d.ts.map
